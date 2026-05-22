99bd7a736ae67223e47a05800b0ead14e85a087d
Fernanda Fajardo <nanda199784@gmail.com>
2026-05-22 07:27:35 -0600

    Inicio de proyecto en clases

---
+++ JN_WEB/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace JN_WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
