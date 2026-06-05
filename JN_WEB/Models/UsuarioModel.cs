ab2fe32a552f6e5480648c4d86f56826e376ae34
Fernanda Fajardo <nanda199784@gmail.com>
2026-06-04 21:10:03 -0600

    Conexión BD con Dapper

--- JN_WEB/Models/UsuarioModel.cs
+++ JN_WEB/Models/UsuarioModel.cs
@@ -1,10 +1,19 @@
-﻿namespace JN_WEB.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JN_WEB.Models
 {
     public class UsuarioModel
     {
+        [Required]
         public string Identificacion {  get; set; } = string.Empty;
+
+        [Required]
         public string Nombre { get; set; } = string.Empty;
+
+        [Required]
         public string CorreoElectronico { get; set; } = string.Empty;
+
+        [Required]
         public string Contrasenna { get; set; } = string.Empty;
     }
 }
